4943114c19c080151d125a258a60d0cb3d42a16d
Muhammad Saad <67402416+MuhammadSaad000@users.noreply.github.com>
2023-01-04 17:24:58 +0500

    business object added

---
+++ ATM_BO/AtmBo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ATM_BO
+{
+    public class AtmBoCust
+    {
+        public int cid { get; set; }
+        public string Name { get; set; }    //login name
+        public int Pin { get; set; }
+        public int Cash { get; set; }
+        public string type { get; set; }
+        public string status { get; set; }  //status of account
+
+    }
+
+    public class AtmBoAd
+    {
+        public int id { get; set; }
+        public string AdminName { get; set; }
+        public int Pin { get; set; }
+    }
+
+
+}
